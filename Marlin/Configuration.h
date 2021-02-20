559d8f44e32f21265bfc2628fc101cc2902d50a1
Isaac W Hanson <isaac@starlig.ht>
2021-02-20 19:48:34 +0000

    G33 delta auto-calibrate settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -656,17 +656,17 @@
   #define DELTA_DIAGONAL_ROD 440.0        // (mm)
 
   // Distance between bed and nozzle Z home position
-  #define DELTA_HEIGHT 437.21             // (mm) Get this value from G33 auto calibrate
+  #define DELTA_HEIGHT 433.84             // (mm) Get this value from G33 auto calibrate
 
-  #define DELTA_ENDSTOP_ADJ { -0.25, 0.0, -0.84 } // Get these values from G33 auto calibrate
+  #define DELTA_ENDSTOP_ADJ { -0.52, 0.0, -0.42 } // Get these values from G33 auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
-  #define DELTA_RADIUS 227.4              // (mm) Get this value from G33 auto calibrate
+  #define DELTA_RADIUS 226.54             // (mm) Get this value from G33 auto calibrate
 
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
   // measured in degrees anticlockwise looking from above the printer
-  #define DELTA_TOWER_ANGLE_TRIM { +0.13, -0.18, +0.05 } // Get these values from G33 auto calibrate
+  #define DELTA_TOWER_ANGLE_TRIM { -0.06, -0.01, +0.07 } // Get these values from G33 auto calibrate
 
   // Delta radius and diagonal rod adjustments (mm)
   //#define DELTA_RADIUS_TRIM_TOWER { 0.0, 0.0, 0.0 }
